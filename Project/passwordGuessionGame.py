fc35c08e6ea13ab61ae359d43a6e6ac1aa308710
hamzabhai797 <hamzanoor11111122@gmail.com>
2026-02-27 10:47:32 +0500

    last project

--- Project/passwordGuessionGame.py
+++ Project/passwordGuessionGame.py
@@ -1,1 +1,12 @@
-# 1:14
+import random
+
+easy_word = ["cat", "dog", "sun", "book", "tree"]
+medium_word = ["python", "guitar", "flower", "mountain", "computer"]
+hard_word = ["elephant", "astronomy", "philosophy", "architecture", "psychology"]
+
+print("Welcome to the Password Guessing Game!")
+print("Choose a difficulty level: easy, medium, hard")
+
+level = input("Enter your choice: ").lower()
+
+# time = 1:23
